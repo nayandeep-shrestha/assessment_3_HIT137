20c593ec25c83e8738b5707fe1ef9bcff7e85956
Rajat Shrestha <rajatshrestha0208@gmail.com>
2026-02-05 19:11:49 +1100

    added gui setup

--- Tkinter.py
+++ Tkinter.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-
+from Image_processing import ImageProcessor
+from Image_History import ImageHistory
 
 class ImageEditorApp:
     def __init__(self, root):
@@ -7,10 +8,30 @@ class ImageEditorApp:
         self.root.title("Image Processing Application")
         self.root.geometry("1000x600")
     
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = ImageEditorApp(root)
-    root.mainloop()
+        self.processor = ImageProcessor()
+        self.history = ImageHistory()
+        self.create_menu ()
+
+    # ---------------- GUI SETUP ---------------- #
+    def create_menu(self):
+        menubar = tk.Menu(self.root)
+ 
+        file_menu = tk.Menu(menubar, tearoff=0)
+        file_menu.add_command(label="Open", command=self.open_image)
+        file_menu.add_command(label="Save", command=self.save_image)
+        file_menu.add_command(label="Save As", command=self.save_as)
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self.root.quit)
+ 
+        edit_menu = tk.Menu(menubar, tearoff=0)
+        edit_menu.add_command(label="Undo", command=self.undo)
+        edit_menu.add_command(label="Redo", command=self.redo)
+ 
+        menubar.add_cascade(label="File", menu=file_menu)
+        menubar.add_cascade(label="Edit", menu=edit_menu)
+        self.root.config(menu=menubar)
+
+
 
  
        
