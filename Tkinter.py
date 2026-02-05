42251932d4dcd59ddc482f5147e8624c874362f3
Rajat Shrestha <rajatshrestha0208@gmail.com>
2026-02-05 19:08:28 +1100

    gui window launch

--- Tkinter.py
+++ Tkinter.py
@@ -1,4 +1,16 @@
-from Image_processing import ImageProcessor
+import tkinter as tk
 
-from Image_History import ImageHistory
 
+class ImageEditorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Processing Application")
+        self.root.geometry("1000x600")
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ImageEditorApp(root)
+    root.mainloop()
+
+ 
+       
